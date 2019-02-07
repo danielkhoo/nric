14006f8af5f9486ccf254e509d7f9dd65a821bd8
Daniel Khoo <danieljkhoo@gmail.com>
2019-02-07 11:35:25 +0800

    added typings

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+declare function validate(input: string): boolean;
+declare function generateNRIC(): string;
+declare function generateFIN(): string;
+export = { validate, generateNRIC, generateFIN };
